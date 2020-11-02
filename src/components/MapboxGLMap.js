7fa1871599b71e3705b4190921a9a1dd19a2dd48
Fredrik bjornland <43269579+Fredrikbjornland@users.noreply.github.com>
2020-11-02 19:01:47 +0100

    Add filters

--- src/components/MapboxGLMap.js
+++ src/components/MapboxGLMap.js
@@ -19,11 +19,250 @@ const MapboxGLMap = () => {
         container: mapContainer.current,
         style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
         center: [10.408773,63.422091],
-        zoom: 10
+        zoom: 30
       });
 
       map.on("load", () => {
         setMap(map);
+        const layers = map.getStyle().layers
+        let labelLayerId
+        for (var i = 0; i < layers.length; i++) {
+          if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+          }
+        }
+        map.addSource('earthquakes', {
+          'type': 'geojson',
+          'data':
+          'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson'
+          });
+           
+          map.addLayer(
+          {
+          'id': 'earthquakes-heat',
+          'type': 'heatmap',
+          'source': 'earthquakes',
+          'maxzoom': 9,
+          'paint': {
+          // Increase the heatmap weight based on frequency and property magnitude
+          'heatmap-weight': [
+          'interpolate',
+          ['linear'],
+          ['get', 'mag'],
+          0,
+          0,
+          6,
+          1
+          ],
+          // Increase the heatmap color weight weight by zoom level
+          // heatmap-intensity is a multiplier on top of heatmap-weight
+          'heatmap-intensity': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          0,
+          1,
+          9,
+          3
+          ],
+          // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+          // Begin color ramp at 0-stop with a 0-transparancy color
+          // to create a blur-like effect.
+          'heatmap-color': [
+          'interpolate',
+          ['linear'],
+          ['heatmap-density'],
+          0,
+          'rgba(33,102,172,0)',
+          0.2,
+          'rgb(103,169,207)',
+          0.4,
+          'rgb(209,229,240)',
+          0.6,
+          'rgb(253,219,199)',
+          0.8,
+          'rgb(239,138,98)',
+          1,
+          'rgb(178,24,43)'
+          ],
+          // Adjust the heatmap radius by zoom level
+          'heatmap-radius': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          0,
+          2,
+          9,
+          20
+          ],
+          // Transition from heatmap to circle layer by zoom level
+          'heatmap-opacity': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          7,
+          1,
+          9,
+          0
+          ]
+          }
+          },
+          'waterway-label'
+          );
+           
+          map.addLayer(
+          {
+          'id': 'earthquakes-point',
+          'type': 'circle',
+          'source': 'earthquakes',
+          'minzoom': 7,
+          'paint': {
+          // Size circle radius by earthquake magnitude and zoom level
+          'circle-radius': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          7,
+          ['interpolate', ['linear'], ['get', 'mag'], 1, 1, 6, 4],
+          16,
+          ['interpolate', ['linear'], ['get', 'mag'], 1, 5, 6, 50]
+          ],
+          // Color circle by earthquake magnitude
+          'circle-color': [
+          'interpolate',
+          ['linear'],
+          ['get', 'mag'],
+          1,
+          'rgba(33,102,172,0)',
+          2,
+          'rgb(103,169,207)',
+          3,
+          'rgb(209,229,240)',
+          4,
+          'rgb(253,219,199)',
+          5,
+          'rgb(239,138,98)',
+          6,
+          'rgb(178,24,43)'
+          ],
+          'circle-stroke-color': 'white',
+          'circle-stroke-width': 1,
+          // Transition from heatmap to circle layer by zoom level
+          'circle-opacity': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          7,
+          0,
+          8,
+          1
+          ]
+          }
+          },
+          'waterway-label'
+          );
+          var hoveredStateId = null;
+        map.addLayer(
+          {
+          'id': '3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 15,
+          'paint': {
+          'fill-extrusion-color': '#aaa',
+           
+          // use an 'interpolate' expression to add a smooth transition effect to the
+          // buildings as the user zooms in
+          'fill-extrusion-height': [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            15,
+            0,
+            15.05,
+            ['get', 'height']
+          ],
+          'fill-extrusion-base': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          15,
+          0,
+          15.05,
+          ['get', 'min_height']
+          ],
+          'fill-extrusion-opacity': 0.6
+          }
+          },
+          labelLayerId
+          );
+          map.addSource('states', {
+            'type': 'geojson',
+            'data':
+            'https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson'
+            });
+             
+            // The feature-state dependent fill-opacity expression will render the hover effect
+            // when a feature's hover state is set to true.
+            map.addLayer({
+            'id': 'state-fills',
+            'type': 'fill',
+            'source': 'states',
+            'layout': {},
+            'paint': {
+            'fill-color': '#627BC1',
+            'fill-opacity': [
+            'case',
+            ['boolean', ['feature-state', 'hover'], false],
+            1,
+            0.5
+            ]
+            }
+            });
+             
+            map.addLayer({
+            'id': 'state-borders',
+            'type': 'line',
+            'source': 'states',
+            'layout': {},
+            'paint': {
+            'line-color': '#627BC1',
+            'line-width': 2
+            }
+            });
+             
+            // When the user moves their mouse over the state-fill layer, we'll update the
+            // feature state for the feature under the mouse.
+            map.on('mousemove', 'state-fills', function (e) {
+            if (e.features.length > 0) {
+            if (hoveredStateId) {
+            map.setFeatureState(
+            { source: 'states', id: hoveredStateId },
+            { hover: false }
+            );
+            }
+            hoveredStateId = e.features[0].id;
+            map.setFeatureState(
+            { source: 'states', id: hoveredStateId },
+            { hover: true }
+            );
+            }
+            });
+             
+            // When the mouse leaves the state-fill layer, update the feature state of the
+            // previously hovered feature.
+            map.on('mouseleave', 'state-fills', function () {
+            if (hoveredStateId) {
+            map.setFeatureState(
+            { source: 'states', id: hoveredStateId },
+            { hover: false }
+            );
+            }
+            hoveredStateId = null;
+            });
         map.resize();
       });
     };
